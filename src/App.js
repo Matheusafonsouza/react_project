fce67ef484716eaa611ef04e7535a5882d5ccd38
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-17 20:20:06 -0300

    feature: Created button who updates projects state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Project 1', 'Project 2', 'Project 3']);
+
+    function handleAddProject() {
+        setProjects([...projects, `New project ${Date.now()}`]);
+    }
+
     return (
         <>
-            <Header title="Homepage">
-                <h2>Bem vindo!</h2>
-            </Header>
-            <Header title="Projects">
-                <h2>Veja nossos projetos!</h2>
-            </Header>
+            <Header title="Homepage" />
+            <ul>
+                {projects.map(project => (
+                    <li key={project}>{project}</li>
+                ))}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar um projeto</button>
         </>
     );
 }
