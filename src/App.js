ae71efb2fdef739207aad43a99af565d713bf803
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-17 20:12:01 -0300

    chore: Created title and children props for Header component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Header from './components/Header';
 function App() {
     return (
         <>
-            <Header />
+            <Header title="Homepage">
+                <h2>Bem vindo!</h2>
+            </Header>
+            <Header title="Projects">
+                <h2>Veja nossos projetos!</h2>
+            </Header>
         </>
     );
 }
