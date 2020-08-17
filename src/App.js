653f29eba07d6e548829da33a3b3f1db87e774ad
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-17 20:07:43 -0300

    feature: Created Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
     return (
-        <h1>Hello!</h1>
+        <>
+            <Header />
+        </>
     );
 }
 
