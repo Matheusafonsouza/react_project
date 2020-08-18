36634aceabd2ba51fa5f95c0366edbbd5de2e82d
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-18 15:43:33 -0300

    chore: Integrated api with front

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
 
@@ -7,7 +8,13 @@ import Image from './assets/photo.jpeg';
 import './App.css';
 
 function App() {
-    const [projects, setProjects] = useState(['Project 1', 'Project 2', 'Project 3']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `New project ${Date.now()}`]);
@@ -21,7 +28,7 @@ function App() {
 
             <ul>
                 {projects.map(project => (
-                    <li key={project}>{project}</li>
+                    <li key={project.id}>{project.title}</li>
                 ))}
             </ul>
 
